f005f22573ec00efc349156533a9ca20e0e6b3ff
hkhs110151 <131329209+hkhs110151@users.noreply.github.com>
2023-04-27 22:34:54 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,19 +3,13 @@ basic.forever(function () {
     desress = input.compassHeading()
     if (desress < 45) {
         basic.showString("N")
+    } else if (desress < 135) {
+        basic.showString("E")
+    } else if (desress < 225) {
+        basic.showString("S")
+    } else if (desress < 315) {
+        basic.showString("W")
     } else {
-        if (desress < 135) {
-            basic.showString("E")
-        } else {
-            if (desress < 225) {
-                basic.showString("S")
-            } else {
-                if (desress < 315) {
-                    basic.showString("W")
-                } else {
-                    basic.showString("N")
-                }
-            }
-        }
+        basic.showString("N")
     }
 })
